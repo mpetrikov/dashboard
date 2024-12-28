898d08b2d4902b48e51d9071e0a0fabbe953f53a
Maksim Petrikov <petrikovmn@gmail.com>
2024-12-29 03:22:51 +0400

    tailwind config

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,18 +1,41 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#5f3df7",
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+            },
+            transitionTimingFunction: {
+                DEFAULT: "ease-in-out",
+            },
+            transitionDuration: {
+                DEFAULT: "350ms",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [
+        plugin(({ addUtilities }) => {
+            addUtilities({
+                ".flex-center-between": {
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                },
+                ".flex-center-center": {
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                },
+            });
+        }),
+    ],
 } satisfies Config;
